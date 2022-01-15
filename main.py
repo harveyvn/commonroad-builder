ddcf3b390e3bc12e065c97802810c3a1d65d9217
Harvey <me@ngvuong.com>
2022-01-15 23:13:23 +0100

    ISS06 - create the function to draw the map

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from modules.crisce import extract_data_from_scenario
 from modules.roadlane import categorize_roadlane
 from modules.analyzer import Analyzer
 from modules.models import Map
-import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    road_data = extract_data_from_scenario("cases/06")
+    road_data = extract_data_from_scenario("cases/01")
     lane_factory = categorize_roadlane(road_data)
     (image, baselines, roads) = lane_factory.run()
 
@@ -15,35 +14,7 @@ if __name__ == '__main__':
         lane_dict = analyzer.search_laneline()
         analyzer.categorize_laneline(lane_dict)
 
-    network = Map(image.shape[0], image.shape[1], roads)
-    plt.imshow(image, cmap="gray")
-    for road in roads:
-        left_boundary = road.left_boundary
-        plt.plot([p[0] for p in list(left_boundary.coords)],
-                 [p[1] for p in list(left_boundary.coords)],
-                 color="blue")
-        for lane in road.lanes[1:-1]:
-            mid_lanes = []
-            color = "tomato"
-            if lane.type == 1:
-                mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio*road.width,
-                                                               side="right", join_style=2))
-            elif lane.type == 3:
-                mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio * road.width - 1.5,
-                                                               side="right", join_style=2))
-                mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio * road.width + 1.5,
-                                                               side="right", join_style=2))
-                color = "red"
-            for mid_lane in mid_lanes:
-                plt.plot([p[0] for p in list(mid_lane.coords)],
-                         [p[1] for p in list(mid_lane.coords)],
-                         color=color,
-                         linestyle="dashed")
+    network = Map(roads, image)
+    network.draw(include_image=True)
 
-        right_boundary = road.right_boundary
-        plt.plot([p[0] for p in list(right_boundary.coords)],
-                 [p[1] for p in list(right_boundary.coords)],
-                 color="green")
-    plt.gca().set_aspect("equal")
-    plt.show()
 
--- modules/models/map.py
+++ modules/models/map.py
@@ -1,11 +1,47 @@
 from .road import Road
+import matplotlib.pyplot as plt
 
 
 class Map:
-    def __init__(self, width: int, height: int, roads: [Road]):
-        self.width = width
-        self.height = height
+    def __init__(self, roads: [Road], image):
+        self.width = image.shape[1]
+        self.height = image.shape[0]
         self.roads = roads
+        self.image = image
+
+    def draw(self, include_image: bool = False):
+        if include_image:
+            plt.imshow(self.image, cmap="gray")
+        plt.title("Map")
+        for road in self.roads:
+            left_boundary = road.left_boundary
+            plt.plot([p[0] for p in list(left_boundary.coords)],
+                     [p[1] for p in list(left_boundary.coords)],
+                     color="blue")
+            for lane in road.lanes[1:-1]:
+                mid_lanes = []
+                color = "tomato"
+                if lane.type == 1:
+                    mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio * road.width,
+                                                                   side="right", join_style=2))
+                elif lane.type == 3:
+                    mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio * road.width - 1.5,
+                                                                   side="right", join_style=2))
+                    mid_lanes.append(left_boundary.parallel_offset(distance=lane.ratio * road.width + 1.5,
+                                                                   side="right", join_style=2))
+                    color = "red"
+                for mid_lane in mid_lanes:
+                    plt.plot([p[0] for p in list(mid_lane.coords)],
+                             [p[1] for p in list(mid_lane.coords)],
+                             color=color,
+                             linestyle="dashed")
+
+            right_boundary = road.right_boundary
+            plt.plot([p[0] for p in list(right_boundary.coords)],
+                     [p[1] for p in list(right_boundary.coords)],
+                     color="green")
+        plt.gca().set_aspect("equal")
+        plt.show()
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)

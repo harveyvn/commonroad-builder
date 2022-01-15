cede4d17b0ca41543634282ba77756c53a0b958c
Harvey <me@ngvuong.com>
2022-01-15 12:30:31 +0100

    ISS03 - searching lines

--- modules/models/analyzer.py
+++ modules/models/analyzer.py
@@ -1,10 +1,13 @@
 import cv2
 import imutils
 import numpy as np
-import matplotlib.pyplot as plt
-from math import floor, ceil
-from modules import slice_when, angle
 from .road import Road
+from .viz_analyzer import VizAnalyzer
+from math import floor, ceil
+from shapely import affinity
+from shapely.geometry import Point, LineString
+from modules import slice_when, angle
+from modules.common import translate_ls_to_new_origin
 from modules.roadlane.laneline import Laneline
 
 
@@ -13,45 +16,12 @@ class Analyzer:
         self.image = image
         self.lanelines = lanelines
         self.road = road
-
-    def draw_img_with_baselines(self, ax):
-        ax.imshow(self.image, cmap='gray')
-        colors = ["red", "green", "blue", "purple"]
-        for i, line in enumerate(self.lanelines):
-            ax.plot([p[0] for p in line.coords],
-                    [p[1] for p in line.coords],
-                    color=colors[i])
-        return ax
-
-    def draw_img_with_roi(self, ax):
-        xs, ys = self.road.poly.exterior.xy
-        ax.imshow(self.image, cmap='gray')
-        ax.plot(xs, ys, c="red")
-        return ax
-
-    def draw_img(self, ax, masked_img):
-        ax.imshow(masked_img, cmap='gray')
-        return ax
-
-    def draw_histogram(self, ax, hist, peaks: [] = None):
-        ax.plot([i for i in range(0, len(hist))], hist)
-        if len(peaks) > 0:
-            for peak in peaks:
-                plt.plot(peak, hist[peak], '.', color="red")
-        return ax
+        self.visualization = VizAnalyzer(image, lanelines, road)
 
     def run(self):
         """
         Detect lanelines from a region of interest
         """
-        fig, ax = plt.subplots(2, 3, figsize=(16, 24))
-        axs = []
-
-        ax[0, 0].title.set_text("Step 01")
-        axs.append(self.draw_img_with_baselines(ax[0, 0]))
-
-        ax[0, 1].title.set_text("Step 02")
-        axs.append(self.draw_img_with_roi(ax[0, 1]))
 
         # Define the bounding rectangle
         # Region of Interest (ROI) to be cropped
@@ -67,15 +37,9 @@ class Analyzer:
         cv2.fillPoly(mask, np.int32([roi_area]), 0)
         # Applying the mask to original image
         masked_img = cv2.bitwise_or(self.image, mask)
-        # Debug
-        ax[0, 2].title.set_text("Step 03")
-        axs.append(self.draw_img(ax[0, 2], masked_img))
 
         # Crop an image
         crop_img = masked_img[ymin:ymax, xmin:xmax]
-        # Debug
-        ax[1, 0].title.set_text("Step 04")
-        axs.append(self.draw_img(ax[1, 0], crop_img))
 
         # Rotate the crop image
         # Find the angle for rotation
@@ -84,48 +48,95 @@ class Analyzer:
         difference = 90 - angle(lineA, lineB)
         # Rotate our image by certain degrees around the center of the image
         rotated_img = imutils.rotate_bound(crop_img, -difference)
+
+        # Visualization: Draw a histogram to find the starting points of lane lines
+        #
+        # fig, ax = plt.subplots(2, 3, figsize=(16, 24))
+        # axs = [self.visualization.draw_img_with_baselines(ax[0, 0], "Step 01"),
+        #        self.visualization.draw_img_with_roi(ax[0, 1], "Step 02"),
+        #        self.visualization.draw_img(ax[0, 2], masked_img, "Step 03"),
+        #        self.visualization.draw_img(ax[1, 0], crop_img, "Step 04"),
+        #        self.visualization.draw_img(ax[1, 1], rotated_img, "Step 05"),
+        #        self.visualization.draw_histogram(ax[1, 2], rotated_img, "Step 06", True)]
+        # plt.show()
+
+        self.road.angle = -difference
+        rotated_lst = affinity.rotate(self.road.mid_line, self.road.angle, (0, 0))
+        # Define variables:
+        # i is a x value, in range (0, rotated_img.shape[1]), aka an end of a rotated image x-axis
+        i = 0
+        is_stop = False
+        invalid_lines, valid_lines = list(), list()
+
+        # Searching invalid lines
+        while is_stop is False:
+            window_line = translate_ls_to_new_origin(rotated_lst, Point(i, 0))
+            if list(window_line.coords)[-1][1] < 0:
+                window_line = translate_ls_to_new_origin(rotated_lst, Point(i, rotated_img.shape[0]))
+
+            # Check if x value of all points (from a line string)
+            # is in a x range of a rotated image or not. Otherwise, the linestring is invalid.
+            invalid_line = False
+            for p in list(window_line.coords):
+                if p[0] < 0:
+                    invalid_line = True
+                    invalid_lines.append({"i": i, "lst": list(window_line.coords)})
+                    i = i + 1
+                    break
+
+            if invalid_line is False:
+                is_stop = True
+
+        # Searching the valid lane ids from remaining lines
+        while i < rotated_img.shape[1]:
+            window_line = translate_ls_to_new_origin(rotated_lst, Point(i, 0))
+            if list(window_line.coords)[-1][1] < 0:
+                window_line = translate_ls_to_new_origin(rotated_lst, Point(i, rotated_img.shape[0]))
+            # Remove a point not in a rotated image
+            coords, checked_coords = list(window_line.coords[0:20]), []
+            for p in coords:
+                try:
+                    if int(rotated_img[int(p[1]), int(p[0])]) > -1:
+                        checked_coords.append(p)
+                except IndexError:
+                    pass
+
+            # Take first 3 points from a line string
+            window_line = LineString(checked_coords[0:3])
+            coords = [(floor(p[0]), floor(p[1])) for p in list(window_line.coords)]
+
+            # Compute the white density from a pixel image
+            lst = list()
+            total = 0
+            for p in coords:
+                lst.append(p)
+                try:
+                    total += int(rotated_img[p[1], p[0]])
+                except IndexError:
+                    pass
+
+            if total > 0:
+                valid_lines.append({"i": i, "lst": lst, "total": total})
+            i = i + 1
+
         # Debug:
-        ax[1, 1].title.set_text("Step 05")
-        axs.append(self.draw_img(ax[1, 1], rotated_img))
+        # self.visualization.draw_searching(invalid_lines, valid_lines, rotated_img)
 
-        # Find a histogram
-        hist = None
-        cutoffs = [int(rotated_img.shape[0] / 2), 0]
-        for cutoff in cutoffs:
-            hist = np.sum(rotated_img[cutoff:, :], axis=0)
-            if hist.max() > 0:
-                break
-        if hist.max() == 0:
-            print('Unable to detect lane lines in this frame. Trying another frame!')
-            return False, np.array([]), np.array([])
-        # Debug:
-        ax[1, 2].title.set_text("Step 06")
-        axs.append(self.draw_histogram(ax[1, 2], hist))
-        plt.show()
+        # Grouping x-values which form a line
+        xs_dict = {}
+        for line in valid_lines:
+            xs_dict[line["i"]] = line["total"]
+        groups = list(slice_when(lambda x, y: y - x > 2, list(xs_dict.keys())))
 
-        threshold = 5500
+        # Compare a total value of each x value to find a peak
         peaks = []
-        for i, val in enumerate(hist):
-            if val > threshold:
-                peaks.append(i)
-        print(f'Possible peaks: {peaks}')
-
-        tst = peaks.copy()
-        slices = list(slice_when(lambda x, y: y - x > 2, tst))
-        print(f'Slice peaks: {slices}')
-        peaks = []
-        for slice in slices:
-            chosen_peak = slice[0]
-            for ind in slice:
-                if hist[ind] > hist[chosen_peak]:
-                    chosen_peak = ind
+        for group in groups:
+            chosen_peak = group[0]
+            for x in group:
+                if xs_dict[x] > xs_dict[chosen_peak]:
+                    chosen_peak = x
             peaks.append(chosen_peak)
-        print(f'Chosen peaks: {peaks}')
 
-        plt.plot([i for i in range(0, len(hist))], hist)
-        for peak in peaks:
-            plt.plot(peak, hist[peak], '.', color="red")
-        plt.show()
-
-        self.road.angle = difference
-        return self.road
+        print(f'Peaks: {peaks}')
+        print(groups)
+        print(xs_dict)

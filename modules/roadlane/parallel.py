2c86f3b3c3335823c8b1bcd7594bdcc0ecf4c18f
Harvey <me@ngvuong.com>
2022-02-06 21:52:28 +0100

    ISS19 - interpolate the base line

--- modules/roadlane/parallel.py
+++ modules/roadlane/parallel.py
@@ -6,9 +6,25 @@ from modules.models import Segment
 from modules.common import find_left_right_boundaries, order_points
 
 
+import numpy.polynomial.polynomial as poly
+
+
 class ParallelLane(RoadLane):
     def process(self):
-        baseline = Laneline(lane_id=0, coords=order_points(self.params["roads"][0]), width=0)
+        coords = order_points(self.params["roads"][0])
+        image = self.params["image"]
+        xs = [p[0] for p in coords]
+        ys = [p[1] for p in coords]
+        coefs = poly.polyfit(xs, ys, 2)
+        poly_xs = [x for x in range(int(xs[0]), int(image.shape[1]))]
+        poly_ys = poly.polyval(poly_xs, coefs)
+        coords = [(x, y) for x, y in zip(poly_xs, poly_ys)]
+        # import matplotlib.pyplot as plt
+        # plt.imshow(image, cmap='gray')
+        # plt.plot(poly_xs, poly_ys, '.')
+        # plt.show()
+
+        baseline = Laneline(lane_id=0, coords=coords, width=0)
         lefts, rights, diff = find_left_right_boundaries(self.params["image"], baseline.get_line())
 
         segment = Segment(road_id=baseline.id,
